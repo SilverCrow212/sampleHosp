f26958f9805b6d665e20f4ee1559e7defef9c091
SilverCrow212 <dixonlexther21@gmail.com>
2024-04-22 21:27:39 +0800

    update errors and installed pinia in main

--- BGHMCEXAMfrontend/src/main.js
+++ BGHMCEXAMfrontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 axios.defaults.baseURL = 'http://127.0.0.1:8000';
+import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import AutoComplete from 'primevue/autocomplete';
 import Accordion from 'primevue/accordion';
@@ -112,7 +113,9 @@ import BlockViewer from '@/components/BlockViewer.vue';
 import '@/assets/styles.scss';
 
 const app = createApp(App);
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(router);
 app.use(PrimeVue, { ripple: true });
 app.use(ToastService);
